ee5165691cd692ec54c68845b2e04469e28946a7
Giorgi <jodasd11@gmail.com>
2022-10-26 21:49:25 +0400

    rust web server

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+#[macro_use]
+extern crate actix_web;
+
+use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
+use serde::Serialize;
+use serde_json::value::Index;
+
+pub struct MessageApp {
+    port: u16,
+}
+
+impl MessageApp {
+    pub fn new(port: u16) -> Self {
+        MessageApp { port }
+    }
+
+    pub fn run(&self) -> std::io::Result<()> {
+        print!("Starting http server: localhost:{}", self.port);
+        HttpServer::new(move || {
+            App::new()
+                .wrap(middleware::Logger::default())
+                .service(index)
+        })
+        .bind(("127.0.0.1", self.port))?
+        .workers(8)
+        .run()
+    }
+}
+
+#[derive(Serialize)]
+struct IndexResponse {
+    message: String,
+}
+
+#[get("/")]
+fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
+    let hello = req.headers()
+    .get("hello")
+    .and_then(|v| v.to_str().ok())
+    .unwrap_or_else(|| "world");
+
+    Ok(web::Json(IndexResponse { message: hello.to_owned(), }))
+}
