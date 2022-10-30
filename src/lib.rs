3542a8991d9ea4477a3fad362f36caa1edf26700
Giorgi <jodasd11@gmail.com>
2022-10-30 19:57:27 +0400

    added app inner state

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,18 @@
 extern crate actix_web;
 
 use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
-use serde::Serialize;
-use serde_json::value::Index;
+use serde::{Deserialize, Serialize};
+use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+struct AppState {
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>
+}
 
 pub struct MessageApp {
     port: u16,
@@ -15,11 +25,23 @@ impl MessageApp {
     }
 
     pub fn run(&self) -> std::io::Result<()> {
+        let messages = Arc::new(Mutex::new(vec![]));
         print!("Starting http server: localhost:{}", self.port);
         HttpServer::new(move || {
             App::new()
+                .data(AppState {
+                    server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+                    request_count: Cell::new(0),
+                    messages: messages.clone(),
+                })
                 .wrap(middleware::Logger::default())
                 .service(index)
+                .service(clear)
+                .service(
+                    web::resource("/send")
+                    .data(web::JsonConfig::default().limit(4096))
+                    .route(web::post().to(post)),
+                )
         })
         .bind(("127.0.0.1", self.port))?
         .workers(8)
@@ -29,15 +51,60 @@ impl MessageApp {
 
 #[derive(Serialize)]
 struct IndexResponse {
+    server_id: usize,
+    request_count: usize,
+    messages: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct PostInput {
     message: String,
 }
 
-#[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-    let hello = req.headers()
-    .get("hello")
-    .and_then(|v| v.to_str().ok())
-    .unwrap_or_else(|| "world");
+#[derive(Serialize)]
+struct PostResponse {
+    server_id: usize,
+    request_count: usize,
+    message: String,
+}
 
-    Ok(web::Json(IndexResponse { message: hello.to_owned(), }))
+fn post(msg: web::Json<PostInput>, state: web::Data<AppState>) -> Result<web::Json<PostResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.push(msg.message.clone());
+
+    Ok(web::Json(PostResponse {
+        server_id: state.server_id,
+        request_count,
+        message: msg.message.clone(),
+    }))
+}
+
+#[post("/clear")]
+fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.clear();
+
+    Ok(web::Json(IndexResponse {
+        server_id: state.server_id,
+        request_count,
+        messages: vec![],
+    }))
+}
+
+
+#[get("/")]
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let ms = state.messages.lock().unwrap();
+
+    Ok(web::Json(IndexResponse{
+        server_id: state.server_id,
+        request_count,
+        messages: ms.clone(),
+    }))
 }
